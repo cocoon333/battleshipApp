8c7130089c467353715727336aeade35551969f9
yong_dev <yong_dev@Zechens-MacBook-Pro.local>
2017-09-20 22:09:10 -0700

    -a

--- model.js
+++ model.js
@@ -11,4 +11,25 @@ Player.prototype.placeShip = function(x, y, direction) {
 
 
 Player.prototype.attack = function(x, y) {
-    this.board.attack(x, y)
+    this.board.attack(x, y);
+}
+
+function Board() {
+    this.boardLen = 10;
+    this.board = [];
+    for (i=0; i<this.boardLen; i++){
+        this.board.push([]);
+        for (j=0; j<this.boardLen; j++){
+            this.board[i].push([0, 0]);
+        }
+    }
+}
+
+Board.prototype.placeShip = function(x, y, direction, size) {
+    if(this.assertShip(x, y, direction, size)){
+        return false;
+    }
+
+    this.board[x][y][0] = 1;
+    if (direction === 0){
+        for (i = 0; i<size; i++){
