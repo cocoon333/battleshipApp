95c81465f286723fa84ed7518205c2c0f0826c92
yong_dev <yong_dev@Zechens-MacBook-Pro.local>
2017-09-25 21:15:29 -0700

    -a

--- model.js
+++ model.js
@@ -25,11 +25,36 @@ function Board() {
     }
 }
 
-Board.prototype.placeShip = function(x, y, direction, size) {
-    if(this.assertShip(x, y, direction, size)){
-        return false;
-    }
+Board.prototype.attack = function(x, y) {
+    this.board[x][y][1] = 1
+}
 
+Board.prototype.placeShip = function(x, y, direction, size) {
     this.board[x][y][0] = 1;
     if (direction === 0){
-        for (i = 0; i<size; i++){
+        for (i = 1; i<size; i++){
+            this.board[x][y-i] = 0;
+        }
+    else if (direction === 1){
+        for (i = 1; i<size; i++){
+            this.board[x+1][y] = 0;
+        }
+    else if (direction === 2){
+        for (i = 1; i<size; i++){
+            this.board[x][y+i] = 0;
+        }
+    else if (direction === 3){
+        for (i = 1; i<size; i++){
+            this.board[x-i][y] = 0;
+        }
+
+function Game() {
+    this.b1 = new Board();
+    this.b2 = new Board();
+    this.p1 = new Player(this.b1);
+    this.p2 = new Player(this.b2);
+}
+
+Game.prototype.initate(b, p){
+    for (i=0; i<5; i++){
+        b.place_ship()
